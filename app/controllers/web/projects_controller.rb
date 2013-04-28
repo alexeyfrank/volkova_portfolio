f52a9d29c60944e2245f99aa2a65bd6f89f2ffb9
Alexey Frank <alexeyfrank@gmail.com>
2013-04-28 21:12:17 +0400

    add code for galleries

--- app/controllers/web/projects_controller.rb
+++ app/controllers/web/projects_controller.rb
@@ -2,4 +2,8 @@ class Web::ProjectsController < Web::ApplicationController
   def index
     @projects = Project.by_created_at
   end
+
+  def show
+    @project = Project.find params[:id]
+  end
 end
