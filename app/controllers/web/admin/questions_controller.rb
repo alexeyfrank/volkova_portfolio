a159c786915d676b697deba9a7f078900f28937f
Alexey Frank <alexeyfrank@gmail.com>
2013-06-28 18:20:35 +0400

    fix questions

--- app/controllers/web/admin/questions_controller.rb
+++ app/controllers/web/admin/questions_controller.rb
@@ -7,4 +7,10 @@ class Web::Admin::QuestionsController < Web::Admin::ApplicationController
   def show
     @question = Question.find params[:id]
   end
+
+  def destroy
+    @question = Question.find params[:id]
+    @question.destroy
+    redirect_to admin_questions_path
+  end
 end
--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -6,7 +6,10 @@ class QuestionMailer < ActionMailer::Base
   def new_question(question, get_catalog)
     @question = question
     if get_catalog
-      attachments['catalog.pdf'] = File.read(Rails.root + '/public/catalog.pdf')
+      begin
+        attachments['catalog.pdf'] = File.read(Rails.root + '/public/catalog.pdf')
+      rescue
+      end
     end
     mail to: configus.admin_email, subject: 'Оставлен новый вопрос'
   end
